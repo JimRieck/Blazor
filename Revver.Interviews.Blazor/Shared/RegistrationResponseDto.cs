e99ad80de93ecd469173a3db1be0390179e0bacb
JimRieck <JimRieck@gmail.com>
2022-11-16 14:32:29 -0500

    Initial Setup of coding challenge.

---
+++ Revver.Interviews.Blazor/Shared/RegistrationResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Revver.Interviews.Blazor.Shared
+{
+    public class RegistrationResponseDto
+    {
+        public bool IsSuccessfulRegistration { get; set; }
+        public IEnumerable<string> Errors { get; set; }
+    }
+}
---
+++ Revver.Interviews.Blazor/Shared/UserRegistrationDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Revver.Interviews.Blazor.Shared
+{
+    public class UserForRegistrationDto
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        [MinLength(12,ErrorMessage = "The minimum length for a password is 12.")]
+        //[RegularExpression("/^[ A-Za-z0-9_@./#&+-]*$/", ErrorMessage="Your password needs letters, numbers and special characters")]
+        public string Password { get; set; }
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
