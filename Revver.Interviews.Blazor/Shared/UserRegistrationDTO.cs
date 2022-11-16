3c24eee6982b919e086e0f91f1dcebf95152eed8
JimRieck <JimRieck@gmail.com>
2022-11-16 17:20:48 -0500

    more changes

--- Revver.Interviews.Blazor/Shared/UserRegistrationDTO.cs
+++ Revver.Interviews.Blazor/Shared/UserRegistrationDTO.cs
@@ -7,8 +7,6 @@ namespace Revver.Interviews.Blazor.Shared
         [Required(ErrorMessage = "Email is required.")]
         public string Email { get; set; }
         [Required(ErrorMessage = "Password is required")]
-        [MinLength(12,ErrorMessage = "The minimum length for a password is 12.")]
-        //[RegularExpression("/^[ A-Za-z0-9_@./#&+-]*$/", ErrorMessage="Your password needs letters, numbers and special characters")]
         public string Password { get; set; }
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
